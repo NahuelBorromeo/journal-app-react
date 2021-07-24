a76d89fed1dd1c78d63d7ce8afe9ab6c63c8f1a4
Nahuel Borromeo <nahuelborromeo@gmail.com>
2021-07-24 13:12:05 -0500

    Updates

---
+++ precache-manifest.1ebd9769fc0bb282c602819613965e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1a3def04e1197d11e0fc51f070f7bd",
+    "url": "/journal-app-react/index.html"
+  },
+  {
+    "revision": "9ad051daa8db8ddf70af",
+    "url": "/journal-app-react/static/css/main.26922b4e.chunk.css"
+  },
+  {
+    "revision": "3d8c52d0d8c60d78b271",
+    "url": "/journal-app-react/static/js/2.235a95a9.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/journal-app-react/static/js/2.235a95a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad051daa8db8ddf70af",
+    "url": "/journal-app-react/static/js/main.2c453203.chunk.js"
+  },
+  {
+    "revision": "1520531804d46453e5bf",
+    "url": "/journal-app-react/static/js/runtime-main.4a614fe8.js"
+  }
+]);
